0dc472e708ba175dac532465de4cf15852df9a1a
Compote0 <pierreorgeret@gmail.com>
2026-06-24 15:27:43 +0200

    tests: update test

--- composeApp/src/commonTest/kotlin/com/amonteiro/a03_kmp_mprolead_g1/presentation/viewmodel/MainViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/com/amonteiro/a03_kmp_mprolead_g1/presentation/viewmodel/MainViewModelTest.kt
@@ -2,6 +2,8 @@ package com.amonteiro.a03_kmp_mprolead_g1.presentation.viewmodel
 
 import com.amonteiro.a03_kmp_mprolead_g1.data.remote.PhotographerAPI
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpResponseValidator
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertTrue
@@ -9,13 +11,20 @@ import kotlin.test.assertTrue
 class MainViewModelTest {
 
     @Test
-    fun testLoadPhotographerRealAPI() = runTest {
-        val client = HttpClient()
+    fun testWhoAlwaysSucceeds() {
+        assertTrue(true, "Ce test passe toujours")
+    }
+
+    @Test
+    fun testPhotographerAPIEchoueSansSecret() = runTest {
+        val client = HttpClient {
+            expectSuccess = true
+        }
+
         val api = PhotographerAPI(client)
-        val viewModel = MainViewModel(api)
 
-        viewModel.loadPhotographer()
+        val result = api.loadPhotographers()
 
-        assertTrue(viewModel.errorMessage.value.isEmpty(), "L'API a renvoyé une erreur : ${viewModel.errorMessage.value}")
+        assertTrue(result.isNotEmpty(), "La liste des photographes ne doit pas être vide")
     }
 }
